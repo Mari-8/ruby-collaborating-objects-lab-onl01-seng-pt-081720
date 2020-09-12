a2eca1fd7e8090dbbf0c1724a2a8fadb2703dbc8
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-12 21:11:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -38,8 +38,12 @@ class Artist
   end 
 
   def self.find_or_create_by_name(name)
-    songs = Song.all
-    songs.find_by_name(name) || self.create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
   end
  
  
